7668e95fbdd37e433798a3a0ba592595a2ed5294
Bharath552-bit <patthembharath32@gmail.com>
2026-02-04 22:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fact=1
+	let count=0
+	let str=n+''
+	for(let i=1;i<=n;i++){
+		fact*=i
+	}
+	for(let i=0;i<str.length;i++){
+		if(str[i]==="0"){
+			count++
+		}
+	}
+	return count
 }
 
 const input = prompt("Enter a number");
